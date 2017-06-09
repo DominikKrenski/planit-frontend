33284007419a0d08a63db057fc465b47cd8442f7
magdalena <magdamrzyglockaa@gmail.com>
2017-06-09 21:37:27 +0200

    change password, edit account not finished yet

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+import {
+  CanActivate, Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+ } from '@angular/router';
+
+import { Subject, Observable } from 'rxjs';
+import { Headers, RequestOptions } from '@angular/http';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'change-password',
@@ -7,9 +17,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  currentUser = "";
+  authToken = localStorage.getItem('currentUser');
+  myinfo = {}
 
-  ngOnInit() {
+  getAuth() {
+      return JSON.parse(localStorage.getItem('currentUser'));
+  }; 
+  constructor(private http:Http, private router: Router) {
+    this.currentUser = this.getAuth();
   }
 
+  ngOnInit() {
+    function getAuth() {
+        return JSON.parse(localStorage.getItem('currentUser'));
+    };    
+  }
+
+  formSubmit = 0;
+
+  userForm = {
+    "OLD_PASSWORD": "",
+    "NEW_PASSWORD": "",
+    "NEW_PASSWORD_CONFIRM": ""
+  };
+
+  server = 'http://planit-backend.com:8888/api/user/change-password';
+
+  save(valid, userForm) {
+    
+    if (valid) {
+      let authToken = localStorage.getItem('currentUser');
+    
+      let token = JSON.parse(authToken);
+
+      let headers = new Headers();
+          headers.append('Authorization', `${token.authorization}`);
+          headers.append('Accept', 'application/json');
+          headers.append('Content-Type', 'application/json');
+
+      let options = new RequestOptions({ headers: headers });
+
+      return this.http.put(this.server, userForm, options)
+        .subscribe( 
+          (res) => {
+            this.formSubmit = 2;
+            this.userForm.OLD_PASSWORD = '';
+            this.userForm.NEW_PASSWORD = '';
+            this.userForm.NEW_PASSWORD_CONFIRM = '';
+          },
+          err => {
+            this.formSubmit = 1;
+            this.userForm.OLD_PASSWORD = '';
+            this.userForm.NEW_PASSWORD = '';
+            this.userForm.NEW_PASSWORD_CONFIRM = '';
+          }
+      );
+    } else {
+      this.formSubmit = 1;
+      this.userForm.OLD_PASSWORD = '';
+      this.userForm.NEW_PASSWORD = '';
+      this.userForm.NEW_PASSWORD_CONFIRM = '';
+      return;
+    }
+  }
 }
--- src/app/edit-account/edit-account.component.ts
+++ src/app/edit-account/edit-account.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+import {
+  CanActivate, Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+ } from '@angular/router';
+
+import { Subject, Observable } from 'rxjs';
+import { Headers, RequestOptions } from '@angular/http';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'edit-account',
@@ -7,9 +17,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditAccountComponent implements OnInit {
 
-  constructor() { }
+  currentUser = "";
+  authToken = localStorage.getItem('currentUser');
+  myinfo = {}
 
-  ngOnInit() {
+  getAuth() {
+      return JSON.parse(localStorage.getItem('currentUser'));
+  }; 
+  constructor(private http:Http, private router: Router) {
+    this.currentUser = this.getAuth();
   }
 
+  ngOnInit() {
+    function getAuth() {
+        return JSON.parse(localStorage.getItem('currentUser'));
+    };    
+  }
+
+  formSubmit = 0;
+
+  userForm = {
+  };
+  
+
+  save(valid, userForm) {
+    
+  }
 }
